5a82c0b7302926fb4f3957e054633496dd65b302
Aurélien PAWLAK <blockedindao@gmail.com>
2024-10-07 19:25:44 +0200

    Forcer la mise à jour du projet

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,6 +1,7 @@
 import { Poppins, Inter, Freckle_Face } from 'next/font/google'
 
 export const poppins = Poppins({
+  weight: ['400', '600', '700'],  // Add the weights you need
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-poppins',
