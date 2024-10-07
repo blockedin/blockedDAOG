e42a56f4154d03c778de3a0ec4b992d00d5cc2ac
Aurélien PAWLAK <blockedindao@gmail.com>
2024-10-07 22:32:53 +0200

    Correction des apostrophes et ajout des polices

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${poppins.variable} font-sans`}>
-      <body>{children}</body>
+    <html lang="en" className={poppins.variable}>
+      <body className="font-sans">{children}</body>
     </html>
   )
 }
